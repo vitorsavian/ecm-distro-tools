0fdafc73c6c2b140a549e24d40499ddf0afcfc65
Vitor Savian <vitor.savian@suse.com>
2025-02-23 22:04:24 -0300

    more stuff

--- cmd/rpm_tooling/main.go
+++ cmd/rpm_tooling/main.go
@@ -4,18 +4,21 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
-
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/smithy-go"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 var version = "development"
@@ -28,6 +31,8 @@ type RpmToolingCmdOpts struct {
 	BucketPrefix string
 	Sign         bool
 	SignPassword string
+
+	S3Client *s3.Client
 }
 
 var rpmToolingCmdOpts RpmToolingCmdOpts
@@ -55,6 +60,38 @@ func main() {
 	}
 }
 
+func (rpmTool *RpmToolingCmdOpts) UploadFile(ctx context.Context, objectKey, fileName string) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Printf("Couldn't open file %v to upload. Here's why: %v\n", fileName, err)
+	} else {
+		defer file.Close()
+		_, err = rpmTool.S3Client.PutObject(ctx, &s3.PutObjectInput{
+			Bucket: aws.String(rpmTool.Bucket),
+			Key:    aws.String(objectKey),
+			Body:   file,
+		})
+		if err != nil {
+			var apiErr smithy.APIError
+			if errors.As(err, &apiErr) && apiErr.ErrorCode() == "EntityTooLarge" {
+				log.Printf("Error while uploading object to %s. The object is too large.\n"+
+					"To upload objects larger than 5GB, use the S3 console (160GB max)\n"+
+					"or the multipart upload API (5TB max).", rpmTool.Bucket)
+			} else {
+				log.Printf("Couldn't upload file %v to %v:%v. Here's why: %v\n",
+					fileName, rpmTool.Bucket, objectKey, err)
+			}
+		} else {
+			err = s3.NewObjectExistsWaiter(rpmTool.S3Client).Wait(
+				ctx, &s3.HeadObjectInput{Bucket: aws.String(rpmTool.Bucket), Key: aws.String(objectKey)}, time.Minute)
+			if err != nil {
+				log.Printf("Failed attempt to wait for object %s to exist.\n", objectKey)
+			}
+		}
+	}
+	return err
+}
+
 func rpmTooling(cmd *cobra.Command, args []string) error {
 	for _, v := range args {
 		if strings.Contains(v, ".rpm") {
@@ -72,15 +109,15 @@ func rpmTooling(cmd *cobra.Command, args []string) error {
 
 	prefixWithRepodata := fmt.Sprintf("%s/%s", rpmToolingCmdOpts.BucketPrefix, "repodata")
 
-	s3Client := s3.NewFromConfig(cfg)
+	rpmToolingCmdOpts.S3Client = s3.NewFromConfig(cfg)
 	input := &s3.ListObjectsV2Input{
 		Bucket: &rpmToolingCmdOpts.Bucket,
 		Prefix: &prefixWithRepodata,
 	}
 
-	_ = fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", rpmToolingCmdOpts.Bucket, rpmToolingCmdOpts.AwsRegion, rpmToolingCmdOpts.BucketPrefix)
+	baseUrl := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", rpmToolingCmdOpts.Bucket, rpmToolingCmdOpts.AwsRegion, rpmToolingCmdOpts.BucketPrefix)
 
-	result, err := s3Client.ListObjectsV2(ctx, input)
+	result, err := rpmToolingCmdOpts.S3Client.ListObjectsV2(ctx, input)
 	var noBucket *types.NoSuchBucket
 	if errors.As(err, &noBucket) {
 		log.Println("creating repodata and a new repo")
@@ -119,36 +156,71 @@ func rpmTooling(cmd *cobra.Command, args []string) error {
 			log.Fatalf("Unable to create %s/repodata: %v", mainDir, "repodata")
 		}
 
+		_, err = exec.Command(
+			`expect -c 'set timeout 60;`,
+			fmt.Sprintf("spawn rpmsign --addsign %s/*;", mainDir),
+			`expect "Passphrase:";`,
+			fmt.Sprintf(`send "%s\\r";`, rpmToolingCmdOpts.SignPassword),
+			"expect eof;",
+		).Output()
+
+		if err != nil {
+			log.Fatalf("Morri no sign xD: %v", err)
+		}
+
+		_, err = exec.Command(
+			`createrepo_c`,
+			`--checksum sha256`,
+			`--baseurl`, baseUrl, mainDir,
+		).Output()
+
+		if err != nil {
+			log.Fatalf("Morri xD: %v", err)
+		}
+
+		_, err = exec.Command(
+			`expect -c 'set timeout 60;`,
+			fmt.Sprintf("spawn gpg --detach-sign --armor %s/repodata/repomd.xml;", mainDir),
+			`expect "Passphrase:";`,
+			fmt.Sprintf(`send "%s\\r";`, rpmToolingCmdOpts.SignPassword),
+			`expect eof;`,
+		).Output()
+
+		if err != nil {
+			log.Fatalf("Morri no gpg xD: %v", err)
+		}
+
+		files, err := os.ReadDir(mainDir)
+		if err != nil {
+			log.Fatalln("Error reading directory: ", err)
+		}
+
+		for _, file := range files {
+			_, err = os.ReadFile(file.Name())
+			if err != nil {
+				fmt.Println("4 real stuff")
+			}
+
+			fmt.Println(file.Name())
+		}
+
+		repodataFiles, err := os.ReadDir(fmt.Sprintf("%s/repodata", mainDir))
+		if err != nil {
+			log.Fatalln("Error reading directory: ", err)
+		}
+
+		for _, file := range repodataFiles {
+			fmt.Println(file.Name())
+		}
+
 	} else if len(result.Contents) > 0 {
-		for i, v := range result.Contents {
-			fmt.Println("------------------------------------------------------")
-			fmt.Println("index: ", i)
-			fmt.Printf("content: %s\n", *v.Key)
-			fmt.Println("------------------------------------------------------")
-		}
-
-		newDir, err := os.MkdirTemp(mainDir, "new_repo")
-		if err != nil {
-			log.Fatalf("Unable to create a temp directory: %v", err)
-		}
-
-		mergeDir, err := os.MkdirTemp(mainDir, "merge_repo")
-		if err != nil {
-			log.Fatalf("Unable to create a temp directory: %v", err)
-		}
-
-		oldDir, err := os.MkdirTemp(mainDir, "old_repo")
-		if err != nil {
-			log.Fatalf("Unable to create a temp directory: %v", err)
-		}
-
-		fmt.Println("------------------------------------------------------")
-		fmt.Printf("temp dir: %s\n", mainDir)
-		fmt.Printf("new dir: %s\n", newDir)
-		fmt.Printf("old dir: %s\n", oldDir)
-		fmt.Printf("merge dir: %s\n", mergeDir)
-		fmt.Println("------------------------------------------------------")
-
+		// for i, v := range result.Contents {
+		// 	fmt.Println("------------------------------------------------------")
+		// 	fmt.Println("index: ", i)
+		// 	fmt.Printf("content: %s\n", *v.Key)
+		// 	fmt.Println("------------------------------------------------------")
+		// }
+		//
 		// newDir, err := os.MkdirTemp(mainDir, "new_repo")
 		// if err != nil {
 		// 	log.Fatalf("Unable to create a temp directory: %v", err)
@@ -159,10 +231,32 @@ func rpmTooling(cmd *cobra.Command, args []string) error {
 		// 	log.Fatalf("Unable to create a temp directory: %v", err)
 		// }
 		//
-		// oldDir, err := os.MkdirTemp(mainDir, "merge_repo")
+		// oldDir, err := os.MkdirTemp(mainDir, "old_repo")
 		// if err != nil {
 		// 	log.Fatalf("Unable to create a temp directory: %v", err)
 		// }
+		//
+		// fmt.Println("------------------------------------------------------")
+		// fmt.Printf("temp dir: %s\n", mainDir)
+		// fmt.Printf("new dir: %s\n", newDir)
+		// fmt.Printf("old dir: %s\n", oldDir)
+		// fmt.Printf("merge dir: %s\n", mergeDir)
+		// fmt.Println("------------------------------------------------------")
+		//
+		// // newDir, err := os.MkdirTemp(mainDir, "new_repo")
+		// // if err != nil {
+		// // 	log.Fatalf("Unable to create a temp directory: %v", err)
+		// // }
+		// //
+		// // mergeDir, err := os.MkdirTemp(mainDir, "merge_repo")
+		// // if err != nil {
+		// // 	log.Fatalf("Unable to create a temp directory: %v", err)
+		// // }
+		// //
+		// // oldDir, err := os.MkdirTemp(mainDir, "merge_repo")
+		// // if err != nil {
+		// // 	log.Fatalf("Unable to create a temp directory: %v", err)
+		// // }
 
 		return nil
 	}
