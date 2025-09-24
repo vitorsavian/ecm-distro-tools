13c1a0c23cc0f132079a92fdcb9b8e0047eedf91
Vitor Savian <vitor.savian@suse.com>
2025-09-24 17:34:46 -0300

    test
    Signed-off-by: Vitor Savian <vitor.savian@suse.com>

--- cmd/rpm/main.go
+++ cmd/rpm/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"slices"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -15,6 +16,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -271,184 +273,42 @@ func copyFile(src, dst string) error {
 	return err
 }
 
-func rpmTool(cmd *cobra.Command, args []string) error {
-	rpmCmdOpts.RpmFiles = args
+func rebuild(client *s3.Client, rpms []types.Object, repodata []types.Object) error {
+	logrus.Info("Rebuild mode enabled. Clearing old, new, and merged repository directories.")
 
-	client, err := createS3Client(rpmCmdOpts.AwsAccessKey, rpmCmdOpts.AwsSecretKey, rpmCmdOpts.AwsRegion)
-	if err != nil {
-		return err
-	}
+	if len(rpms) > 0 {
+		logrus.Infof("Found %d items in S3 bucket %s with prefix %s", len(rpms), rpmCmdOpts.Bucket, rpmCmdOpts.Prefix)
+		for _, item := range rpms {
+			relativePath := *item.Key
 
-	if err := os.MkdirAll(oldRepoPath, 0777); err != nil {
-		return err
-	}
+			// this is for when prefix is set, mostly to avoid the need to handle newRepoPath+prefix+"/"+file in newRepoPath
+			if rpmCmdOpts.Prefix != "" {
+				relativePath = (*item.Key)[len(rpmCmdOpts.Prefix)+1:]
+			}
 
-	if err := os.MkdirAll(newRepoPath, 0777); err != nil {
-		return err
-	}
+			localPath := filepath.Join(newRepoPath, relativePath)
+			if err := downloadS3Object(client, rpmCmdOpts.Bucket, *item.Key, localPath); err != nil {
+				return err
+			}
+		}
 
-	if rpmCmdOpts.Rebuild {
-		logrus.Info("Rebuild mode enabled. Clearing old, new, and merged repository directories.")
-		rpms, err := listS3Objects(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix)
-		if err != nil {
+		logrus.Info("Old RPMs downloaded from S3.")
+		logrus.Info("Running createrepo_c to rebuild RPMs.")
+		comd := exec.Command("createrepo_c", "--checksum", "sha256", newRepoPath)
+		if err := comd.Run(); err != nil {
 			return err
 		}
-
-		if len(rpms) > 0 {
-			logrus.Infof("Found %d items in S3 bucket %s with prefix %s", len(rpms), rpmCmdOpts.Bucket, rpmCmdOpts.Prefix)
-			for _, item := range rpms {
-				relativePath := *item.Key
-
-				// this is for when prefix is set, mostly to avoid the need to handle newRepoPath+prefix+"/"+file in newRepoPath
-				if rpmCmdOpts.Prefix != "" {
-					relativePath = (*item.Key)[len(rpmCmdOpts.Prefix)+1:]
-				}
-
-				localPath := filepath.Join(newRepoPath, relativePath)
-				if err := downloadS3Object(client, rpmCmdOpts.Bucket, *item.Key, localPath); err != nil {
-					return err
-				}
-			}
-
-			logrus.Info("Old RPMs downloaded from S3.")
-			logrus.Info("Running createrepo_c to rebuild RPMs.")
-			comd := exec.Command("createrepo_c", "--checksum", "sha256", newRepoPath)
-			if err := comd.Run(); err != nil {
-				return err
-			}
-
-			if rpmCmdOpts.Sign {
-				logrus.Info("Signing new repository metadata.")
-				if err := signRepo(rpmCmdOpts.SignPass, newRepoPath); err != nil {
-					return err
-				}
-			}
-
-			// get the list of repodata files to delete later and clean the repodata
-			repodata, err := listS3Objects(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix+"/repodata")
-			if err != nil {
-				return err
-			}
-
-			// first we upload and after that the tool will delete the unnecessary items that we got in the list
-			if err := uploadDirectory(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix, newRepoPath, rpmCmdOpts.Visibility); err != nil {
-				return err
-			}
-
-			var keysToDelete []string
-			for _, item := range repodata {
-				if strings.Contains(*item.Key, "repomd") {
-					logrus.Infof("Skipping deletion of repomd file: %s", *item.Key)
-					continue
-				}
-
-				keysToDelete = append(keysToDelete, *item.Key)
-			}
-
-			logrus.Infof("Deleting old repodata files from S3: %v", keysToDelete)
-			if err := deleteS3Objects(client, rpmCmdOpts.Bucket, keysToDelete); err != nil {
-				return err
-			}
-
-		} else {
-			logrus.Info("No existing RPMs found in S3.")
-		}
-
-		return nil
-	}
-
-	if len(rpmCmdOpts.RpmFiles) == 0 {
-		return errors.New("at least one RPM file must be provided")
-	}
-
-	var destRpms []string
-	for _, rpmFile := range rpmCmdOpts.RpmFiles {
-		if rpmCmdOpts.Sign {
-			logrus.Infof("Signing %s", rpmFile)
-			if err := sign(rpmCmdOpts.SignPass, rpmFile); err != nil {
-				return err
-			}
-		}
-
-		basename := filepath.Base(rpmFile)
-		localDest := filepath.Join(newRepoPath, basename)
-		logrus.Infof("Copying %s to %s", rpmFile, localDest)
-		destRpms = append(destRpms, localDest)
-		if err := copyFile(rpmFile, localDest); err != nil {
-			return err
-		}
-	}
-
-	logrus.Info("Running createrepo_c for new RPMs only.")
-	comd := exec.Command("createrepo_c", "--checksum", "sha256", newRepoPath)
-	if err := comd.Run(); err != nil {
-		return err
-	}
-
-	repodataNew := filepath.Join(newRepoPath, "repodata")
-	repomdNew := filepath.Join(repodataNew, "repomd.xml")
-
-	logrus.Infof("Repodata created at: %s", repodataNew)
-	logrus.Infof("Repomd.xml location: %s", repomdNew)
-
-	repodata, err := listS3Objects(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix+"/repodata")
-	if err != nil {
-		return err
-	}
-
-	if len(repodata) > 0 {
-		logrus.Infof("Found %d items in S3 bucket %s with prefix %s", len(repodata), rpmCmdOpts.Bucket, rpmCmdOpts.Prefix+"repodata")
-		for _, item := range repodata {
-			localPath := filepath.Join(oldRepoPath, "repodata")
-			itemPath := filepath.Join(localPath, filepath.Base(*item.Key))
-			if err := downloadS3Object(client, rpmCmdOpts.Bucket, *item.Key, itemPath); err != nil {
-				return err
-			}
-		}
-
-		logrus.Info("Creating mergepo folder to merge old + new repos")
-		if err := os.MkdirAll(mergedRepoPath, 0777); err != nil {
-			return err
-		}
-
-		mergeRepoScriptCmd := exec.Command("mergerepo_c",
-			"--repo="+oldRepoPath,
-			"--repo="+newRepoPath,
-			"--all",
-			"--omit-baseurl",
-			"-o", mergedRepoPath)
-
-		if err := mergeRepoScriptCmd.Run(); err != nil {
-			return fmt.Errorf("failed to merge repositories: %w", err)
-		}
-
-		repodataMerged := filepath.Join(mergedRepoPath, "repodata")
-		repomdMerged := filepath.Join(repodataMerged, "repomd.xml")
-
-		logrus.Infof("Merged repodata created at: %s", repodataMerged)
-		logrus.Infof("Merged repomd.xml location: %s", repomdMerged)
 
 		if rpmCmdOpts.Sign {
-			logrus.Info("Signing merged repository metadata.")
-			if err := signRepo(rpmCmdOpts.SignPass, mergedRepoPath); err != nil {
+			logrus.Info("Signing new repository metadata.")
+			if err := signRepo(rpmCmdOpts.SignPass, newRepoPath); err != nil {
 				return err
 			}
 		}
 
 		// first we upload and after that the tool will delete the unnecessary items that we got in the list
-		if err := uploadDirectory(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix, mergedRepoPath, rpmCmdOpts.Visibility); err != nil {
+		if err := uploadDirectory(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix, newRepoPath, rpmCmdOpts.Visibility); err != nil {
 			return err
-		}
-
-		for _, rpm := range destRpms {
-			s3Key := filepath.Base(rpm)
-			if rpmCmdOpts.Prefix != "" {
-				s3Key = rpmCmdOpts.Prefix + "/" + s3Key
-			}
-
-			if err := uploadS3Object(client, rpmCmdOpts.Bucket, s3Key, rpm, rpmCmdOpts.Visibility); err != nil {
-				return err
-			}
 		}
 
 		var keysToDelete []string
@@ -467,19 +327,174 @@ func rpmTool(cmd *cobra.Command, args []string) error {
 		}
 
 	} else {
-		logrus.Info("No existing repodata found in S3. Uploading new RPMs and repodata.")
+		logrus.Info("No existing RPMs found in S3.")
+	}
 
+	return nil
+}
+
+func mergerepo(client *s3.Client, repodata []types.Object, newRpms []string) error {
+	logrus.Infof("Found %d items in S3 bucket %s with prefix %s", len(repodata), rpmCmdOpts.Bucket, rpmCmdOpts.Prefix+"repodata")
+	for _, item := range repodata {
+		localPath := filepath.Join(oldRepoPath, "repodata")
+		itemPath := filepath.Join(localPath, filepath.Base(*item.Key))
+		if err := downloadS3Object(client, rpmCmdOpts.Bucket, *item.Key, itemPath); err != nil {
+			return err
+		}
+	}
+
+	logrus.Info("Creating mergepo folder to merge old + new repos")
+	if err := os.MkdirAll(mergedRepoPath, 0777); err != nil {
+		return err
+	}
+
+	mergeRepoScriptCmd := exec.Command("mergerepo_c",
+		"--repo="+oldRepoPath,
+		"--repo="+newRepoPath,
+		"--all",
+		"--omit-baseurl",
+		"-o", mergedRepoPath)
+
+	if err := mergeRepoScriptCmd.Run(); err != nil {
+		return fmt.Errorf("failed to merge repositories: %w", err)
+	}
+
+	repodataMerged := filepath.Join(mergedRepoPath, "repodata")
+	repomdMerged := filepath.Join(repodataMerged, "repomd.xml")
+
+	logrus.Infof("Merged repodata created at: %s", repodataMerged)
+	logrus.Infof("Merged repomd.xml location: %s", repomdMerged)
+
+	if rpmCmdOpts.Sign {
+		logrus.Info("Signing merged repository metadata.")
+		if err := signRepo(rpmCmdOpts.SignPass, mergedRepoPath); err != nil {
+			return err
+		}
+	}
+
+	// first we upload and after that the tool will delete the unnecessary items that we got in the list
+	if err := uploadDirectory(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix, mergedRepoPath, rpmCmdOpts.Visibility); err != nil {
+		return err
+	}
+
+	for _, rpm := range newRpms {
+		s3Key := filepath.Base(rpm)
+		if rpmCmdOpts.Prefix != "" {
+			s3Key = rpmCmdOpts.Prefix + "/" + s3Key
+		}
+
+		if err := uploadS3Object(client, rpmCmdOpts.Bucket, s3Key, rpm, rpmCmdOpts.Visibility); err != nil {
+			return err
+		}
+	}
+
+	var keysToDelete []string
+	for _, item := range repodata {
+		if strings.Contains(*item.Key, "repomd") {
+			logrus.Infof("Skipping deletion of repomd file: %s", *item.Key)
+			continue
+		}
+
+		keysToDelete = append(keysToDelete, *item.Key)
+	}
+
+	logrus.Infof("Deleting old repodata files from S3: %v", keysToDelete)
+	if err := deleteS3Objects(client, rpmCmdOpts.Bucket, keysToDelete); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func rpmTool(cmd *cobra.Command, args []string) error {
+	rpmCmdOpts.RpmFiles = args
+
+	client, err := createS3Client(rpmCmdOpts.AwsAccessKey, rpmCmdOpts.AwsSecretKey, rpmCmdOpts.AwsRegion)
+	if err != nil {
+		return err
+	}
+
+	rpms, err := listS3Objects(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix)
+	if err != nil {
+		return err
+	}
+
+	for _, rpm := range rpms {
+		logrus.Infof("Found existing RPM in S3: %s", *rpm.Key)
+
+		if slices.Contains(rpmCmdOpts.RpmFiles, filepath.Base(*rpm.Key)) {
+			return fmt.Errorf("RPM %s already exists in S3 bucket %s with prefix %s", filepath.Base(*rpm.Key), rpmCmdOpts.Bucket, rpmCmdOpts.Prefix)
+		}
+
+	}
+
+	return nil
+
+	repodata, err := listS3Objects(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix+"/repodata")
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(oldRepoPath, 0777); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(newRepoPath, 0777); err != nil {
+		return err
+	}
+
+	if rpmCmdOpts.Rebuild {
+		return rebuild(client, rpms, repodata)
+	}
+
+	if len(rpmCmdOpts.RpmFiles) == 0 {
+		return errors.New("at least one RPM file must be provided")
+	}
+
+	var newRpms []string
+	for _, rpmFile := range rpmCmdOpts.RpmFiles {
 		if rpmCmdOpts.Sign {
-			logrus.Info("Signing new repository metadata.")
-			if err := signRepo(rpmCmdOpts.SignPass, newRepoPath); err != nil {
+			logrus.Infof("Signing %s", rpmFile)
+			if err := sign(rpmCmdOpts.SignPass, rpmFile); err != nil {
 				return err
 			}
 		}
 
-		uploadDirectory(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix, newRepoPath, rpmCmdOpts.Visibility)
+		basename := filepath.Base(rpmFile)
+		localDest := filepath.Join(newRepoPath, basename)
+		logrus.Infof("Copying %s to %s", rpmFile, localDest)
+		newRpms = append(newRpms, localDest)
+		if err := copyFile(rpmFile, localDest); err != nil {
+			return err
+		}
 	}
 
-	return nil
+	logrus.Info("Running createrepo_c for new RPMs only.")
+	comd := exec.Command("createrepo_c", "--checksum", "sha256", newRepoPath)
+	if err := comd.Run(); err != nil {
+		return err
+	}
+
+	repodataNew := filepath.Join(newRepoPath, "repodata")
+	repomdNew := filepath.Join(repodataNew, "repomd.xml")
+
+	logrus.Infof("Repodata created at: %s", repodataNew)
+	logrus.Infof("Repomd.xml location: %s", repomdNew)
+
+	if len(repodata) > 0 {
+		return mergerepo(client, repodata, newRpms)
+	}
+
+	logrus.Info("No existing repodata found in S3. Uploading new RPMs and repodata.")
+
+	if rpmCmdOpts.Sign {
+		logrus.Info("Signing new repository metadata.")
+		if err := signRepo(rpmCmdOpts.SignPass, newRepoPath); err != nil {
+			return err
+		}
+	}
+
+	return uploadDirectory(client, rpmCmdOpts.Bucket, rpmCmdOpts.Prefix, newRepoPath, rpmCmdOpts.Visibility)
 }
 
 func main() {
